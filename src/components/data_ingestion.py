dd3654e1660a2785412bbda4446f0f8d7d543c5c
aman-srma <aman.srma.a7@gmail.com>
2024-08-16 11:42:47 +0530

    data ingestion file created

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,51 @@
+import os, sys
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+from src.logger import logging
+from  src.exception import CustomException
+from dataclasses import dataclass
+
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path = os.path.join("artifacts/data_ingestion", "train.csv")
+    test_data_path = os.path.join("artifacts/data_ingestion", "test.csv")
+    original_data_path = os.path.join("artifacts/data_ingestion", "original.csv")
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Data Ingestion Has Started")
+
+        try:
+            data = pd.read_csv(os.path.join("notebook/data", "income_cleandata.csv"))
+
+            os.makedirs(os.path.dirname(self.ingestion_config.original_data_path), exist_ok=True)
+            data.to_csv(self.ingestion_config.original_data_path, index=False)
+
+            train_set, test_set = train_test_split(data, test_size=0.25, random_state=7)
+
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            train_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
+            logging.info("Data Ingestion Has Been Completed")
+
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path,
+            )
+
+        except Exception as e:
+            logging.info("Error Has Occurred In Data Ingestion Stage")
+            raise CustomException(e, sys)
+
+
+if __name__ =="__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
